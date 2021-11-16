879fb2aab3e63f80d139e8d7403ac1f3c97040b7
Andrii Sergeiev <avsergeyev@gmail.com>
2021-11-16 22:32:56 +0200

    Update docusaurus.config.js

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -80,6 +80,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'intro',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
