a5df3c2a50573c49c7fc127f82d9ee5ccc4bda67
Andrii Sergeiev <avsergeyev@gmail.com>
2021-11-17 13:47:54 +0200

    roll back

---
+++ docusaurus/sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+    someSidebar:[
+            'intro',
+            'how-to-use',
+            'face-services-and-plugins',
+            'rest-api-description',
+            'face-recognition-similarity-threshold',
+            'configuration',
+            'architecture-and-scalability',
+            'custom-builds',
+            'face-data-migration',
+            'user-roles-system',
+            'mask-detection-plugin',
+            'gathering-anonymous-statistics',
+    ],
+};
